5365cafcfca6cf7e14e77a6653291b66432a513d
Len Woodward <Len@ProjektGopher.com>
2021-10-02 19:04:08 -0700

    add tailwindcss and typography plugin via cdn

--- resources/views/standalone.blade.php
+++ resources/views/standalone.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Blog Title</title>
+
+    <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" />
+    <link rel="stylesheet" href="https://unpkg.com/@tailwindcss/typography@0.4.x/dist/typography.min.css" />
 </head>
 <body>
     {!! $body !!}
